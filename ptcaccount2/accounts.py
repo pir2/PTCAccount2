0adad7659c1ad09a5d62f3ca34a211c6673602b2
pir2 <pir2@users.noreply.github.com>
2016-08-16 19:18:13 -0400

    Update accounts.py

--- ptcaccount2/accounts.py
+++ ptcaccount2/accounts.py
@@ -230,7 +230,7 @@ def random_account(username=None, password=None, email=None, birthday=None):
             else:
                 raise
 
-    ActivateEmail(username)
+    emailactivation.ActivateEmail(username)
 
     return {
         "username": try_username,
